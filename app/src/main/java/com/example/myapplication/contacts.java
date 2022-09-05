32384068fbca206ce9b5fcea5b5c3766f4632dc7
yehonatan hacham <yehonatan.hacham@gmial.com>
2022-09-06 02:54:02 +0300

    adapt+close to finish

--- app/src/main/java/com/example/myapplication/contacts.java
+++ app/src/main/java/com/example/myapplication/contacts.java
@@ -2,16 +2,41 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
-public class contacts extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class contacts extends AppCompatActivity implements AdapterView.OnItemLongClickListener, AdapterView.OnItemClickListener {
+    ListView fllv2;
+    ArrayList<contc> alfl2;
+    contadapat AAFL2;
+    SharedPreferences ap2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contacts);
 
+        fllv2 = findViewById(R.id.lv);
+        alfl2 = new ArrayList<>();
+        AAFL2 = new contadapat(this,0,0,alfl2);
+        fllv2.setAdapter(AAFL2);
+        fllv2.setOnItemLongClickListener(this);
+        fllv2.setOnItemClickListener(this);
 
+    }
+
+    @Override
+    public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+        return true;
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
 
     }
 }
--- app/src/main/java/com/example/myapplication/register.java
+++ app/src/main/java/com/example/myapplication/register.java
@@ -3,32 +3,54 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.ParcelUuid;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class register extends AppCompatActivity implements View.OnClickListener {
 private EditText t1;
 private EditText t2;
 private Button b1;
 private Button b2;
+private EditText t3;
+private SharedPreferences aads;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
         t1 = findViewById(R.id.vriscool);
         t2 = findViewById(R.id.imdoingthisinvr);
+        t3 = findViewById(R.id.lname);
         b1=findViewById(R.id.submit);
         b2=findViewById(R.id.changevis);
         b1.setOnClickListener(this);
         b2.setOnClickListener(this);
+        getthisMF();
     }
 
+
+    public void getthisMF(){
+    t1.setText(aads.getString("id:",""));
+        t2.setText(aads.getString("fname:",""));
+        t3.setText(aads.getString("lname:",""));
+
+
+
+    }
     @Override
     public void onClick(View view2) {
         if(view2==b1){
             b2.setVisibility(View.VISIBLE);
+            SharedPreferences.Editor spl1 = aads.edit();
+            spl1.putString("id:",t1.getText().toString());
+            spl1.putString("fname:",t2.getText().toString());
+            spl1.putString("lname:",t3.getText().toString());
+            spl1.commit();
+            Toast.makeText(this,"data saved",Toast.LENGTH_LONG);
         }else {
             startActivity(new Intent(register.this,contacts.class));
         }
