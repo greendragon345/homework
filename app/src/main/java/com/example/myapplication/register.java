bac5845b03f97a51931bd422f340ee39197e38d9
yehonatan hacham <yehonatan.hacham@gmial.com>
2022-09-05 21:44:25 +0300

    dassa

--- app/src/main/java/com/example/myapplication/register.java
+++ app/src/main/java/com/example/myapplication/register.java
@@ -3,12 +3,33 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class register extends AppCompatActivity {
-
+public class register extends AppCompatActivity implements View.OnClickListener {
+private EditText t1;
+private EditText t2;
+private Button b1;
+private Button b2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        t1 = findViewById(R.id.vriscool);
+        t2 = findViewById(R.id.imdoingthisinvr);
+        b1=findViewById(R.id.submit);
+        b2=findViewById(R.id.changevis);
+        b1.setOnClickListener(this);
+        b2.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view2) {
+        if(view2==b1){
+            b2.setVisibility(View.VISIBLE);
+        }else {
+            b
+        }
     }
 }
