058bec760e28dac757084df19b980ae6dbd894b5
yehonatan hacham <yehonatan.hacham@gmial.com>
2022-09-05 21:45:55 +0300

    dassa

--- app/src/main/java/com/example/myapplication/register.java
+++ app/src/main/java/com/example/myapplication/register.java
@@ -2,6 +2,7 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -29,7 +30,7 @@ private Button b2;
         if(view2==b1){
             b2.setVisibility(View.VISIBLE);
         }else {
-            b
+            startActivity(new Intent(register.this,contacts.class));
         }
     }
 }
